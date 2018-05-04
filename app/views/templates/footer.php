b089d59765695f47922f2fb4fb963a4123099a32
oscar0812 <oscar0812torres@gmail.com>
2018-05-03 23:34:26 -0500

    no copyright

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -142,14 +142,10 @@
 <div id="copyright">
   <div class="container">
     <div class="col-md-6">
-      <p class="pull-left">© 2015 Your name goes here.</p>
+      <p class="pull-left">© 2018 BookStoreInc.</p>
 
     </div>
-    <div class="col-md-6">
-      <p class="pull-right">Template by <a href="https://bootstrapious.com/e-commerce-templates">Bootstrapious</a> & <a href="https://fity.cz">Fity</a>
-        <!-- Not removing these links is part of the license conditions of the template. Thanks for understanding :) If you want to use the template without the attribution links, you can do so after supporting further themes development at https://bootstrapious.com/donate  -->
-      </p>
-    </div>
+    <!-- no copyright since donation has been given -->
   </div>
 </div>
 <!-- *** COPYRIGHT END *** -->
